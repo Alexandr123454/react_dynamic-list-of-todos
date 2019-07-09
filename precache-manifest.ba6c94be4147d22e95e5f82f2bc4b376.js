b7b0144c246f507bb396b6c15b72831ca654353f
alexandr <alexandrov6927@gmail.com>
2019-07-09 14:08:58 +0300

    Updates

---
+++ precache-manifest.ba6c94be4147d22e95e5f82f2bc4b376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d594dd2d4ca71097af6feabadd025b",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "e9d3a63ba2145a44c439",
+    "url": "/react_dynamic-list-of-todos/static/css/main.210c3327.chunk.css"
+  },
+  {
+    "revision": "b5a7d8581cf920bad2d0",
+    "url": "/react_dynamic-list-of-todos/static/js/2.b11565d8.chunk.js"
+  },
+  {
+    "revision": "e9d3a63ba2145a44c439",
+    "url": "/react_dynamic-list-of-todos/static/js/main.dc6170e8.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
